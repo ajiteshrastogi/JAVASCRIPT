7cc5357f32c1711c32770fb98d9fb2833d481ed1
ajiteshrastogi <ajiteshrastogi19@gmail.com>
2025-01-29 16:13:49 +0530

    js

--- basic.js
+++ basic.js
@@ -62,35 +62,3 @@ const Student = {
 };
 console.log(Student.naam +"\n" + Student.age);
 console.log(Student["add"] , Student["cgpa"]);
-
-
-//loop
-// for...of Loop
-// Purpose: Used to iterate over iterable objects (like arrays, strings, maps, sets, and more).
-// Use case: Ideal for working with values directly in iterable collections.
-
-// just like for each loop
-
-const numbers = [10, 20, 30];
-for (let num of numbers) {
-    console.log(num);
-}
-// Output:
-// 10
-// 20
-// 30
-
-// for...in Loop
-// Purpose: Used to iterate over the enumerable properties of an object.
-// Use case: Ideal for objects or when you need to iterate over keys (property names).
-// it iterates over the indexing.
-
-const arr = [10, 20, 30];
-for (let index in arr) {
-    console.log(index, arr[index]);
-}
-
-// Output:
-// 0 10
-// 1 20
-// 2 30
