a6e68dd2275379ac9e711d11c596e2205c8113c9
ajiteshrastogi <ajiteshrastogi19@gmail.com>
2025-03-19 23:36:30 +0530

    Basics of JS

---
+++ 01_BASICS/4_GuesstheNum.js
@@ -0,0 +1,8 @@
+let num = 25; 
+let Usernum = prompt("Guess your number :");
+
+while (Usernum != num) {
+    Usernum = prompt("Sorry, you are wrong!\nTry Again:");
+}
+
+alert("Hurray! You Won 🎉\nYour number is correct!");
