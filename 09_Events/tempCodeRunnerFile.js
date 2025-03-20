d391287f69d60ca816a6d92c7dd5154e1432abb1
ajiteshrastogi <ajiteshrastogi19@gmail.com>
2025-03-20 20:08:22 +0530

    Events

---
+++ 09_Events/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+/ ===================== 9. Removing Event Listeners =====================
+// We can remove an event listener using removeEventListener.
+
+function sayHello() {
+    alert("Hello!");
+}
